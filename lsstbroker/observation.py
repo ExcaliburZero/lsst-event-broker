98163f25dfe7e63281bf856745381be89dfa5a83
Christian Sumano <csumano@oswego.edu>
2016-11-12 12:46:53 -0500

    Implement and rename Observation
    Implement and rename the Observation class. Also change it to include error rather than color.
    
    Closes #9

---
+++ lsstbroker/observation.py
@@ -0,0 +1,23 @@
+"""
+This class will get the time value and error observation from each transient
+and store that data into each specific transient. Afterwards this data can be used
+to classify the data based on this information.
+"""
+
+
+class Observation(object):
+
+    time = None
+    light = None
+    error = None
+
+    def __init__(self, time, light_value, error):
+        """
+        this method is to be called on by the event broker when it wants
+        to geneerate a transients time, light-value and error, it will return
+        a tuple.
+        """
+
+        self.time = time
+        self.light = light_value
+        self.error = error
