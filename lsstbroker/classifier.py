05f921e31fea6b95d3d67bda8404c01cf06735e1
Christopher Wells <cwellsny@nycap.rr.com>
2016-11-13 15:44:34 -0500

    Implement determining classifiers
    Implement determining classifiers in the Classifier class. Also add tests for the new method implementations.
    
    Additionally, update the uml diagram to include the new changes.

--- lsstbroker/classifier.py
+++ lsstbroker/classifier.py
@@ -4,16 +4,24 @@ class Classifier(object):
     """
 
     determining_classifier = None
+    continue_function = None
     binary_classifiers = None
 
     def __init__(self):
         self.binary_classifiers = []
 
-    def set_determining_classifier(self, binary_classifier):
+    def set_determining_classifier(self, determining_classifier, continue_function):
         """
-        Sets the given BinaryClassifier to be the Classifier's Determining Classifier.
+        Sets the determining classifier and continue function of the Classifier, these are used to determine whether the
+        observation data should be run through all of the binary classifiers or not.
+
+        :param determining_classifier: A Binary Classifier that is used in determining whether to run the rest of the
+        BinaryClassifiers.
+        :param continue_function: A function that takes the results of the determining classifier and tells whether to
+        run the rest of the BinaryClassifiers.
         """
-        raise NotImplementedError("Have not yet implemented Determining Classifiers")
+        self.determining_classifier = determining_classifier
+        self.continue_function = continue_function
 
     def add_binary_classifier(self, binary_classifier):
         """
@@ -27,6 +35,16 @@ class Classifier(object):
         Runs the given Observation data through the Classifier's BinaryClassifiers and returns the results as a list.
         """
         results = []
-        for binary_classifier in self.binary_classifiers:
-            results.append(binary_classifier.run(observations))
+        run_all_classifiers = True
+        if self.determining_classifier is not None:
+            determining_result = self.determining_classifier.run(observations)
+            results.append(determining_result)
+            should_continue = self.continue_function(determining_result)
+            if not should_continue:
+                run_all_classifiers = False
+
+        if run_all_classifiers:
+            for binary_classifier in self.binary_classifiers:
+                results.append(binary_classifier.run(observations))
+
         return results
--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -23,16 +23,25 @@ class TestClassifier(unittest.TestCase):
         self.assertEquals([], test_classifier2.binary_classifiers)
 
     def test_set_determining_classifier(self):
-        def function(_): return 0.5
-        test_binary_classifier = binary_classifier.BinaryClassifier("001", function)
+        def function(_): return 0.7
+        test_determining_classifier = binary_classifier.BinaryClassifier("D001", function)
+
+        def continue_function(x):
+            (_, value) = x
+            return value > 0.5
         test_classifier = classifier.Classifier()
 
-        with self.assertRaises(NotImplementedError) as context:
-            test_classifier.set_determining_classifier(test_binary_classifier)
+        test_classifier.set_determining_classifier(test_determining_classifier, continue_function)
 
-        self.assertTrue("Have not yet implemented Determining Classifiers" in context.exception)
+        self.assertEquals(test_determining_classifier, test_classifier.determining_classifier)
+        self.assertEquals(continue_function, test_classifier.continue_function)
 
-    def test_run(self):
+        # Run with just a determining classifier
+        no_observations = []
+        expected_results = [("D001", 0.7)]
+        self.assertEquals(expected_results, test_classifier.run(no_observations))
+
+    def test_run_no_determining(self):
         def function(_): return 0.7
         test_binary_classifier = binary_classifier.BinaryClassifier("001", function)
         test_classifier = classifier.Classifier()
@@ -52,3 +61,31 @@ class TestClassifier(unittest.TestCase):
         results2 = [("001", 0.7), ("002", 0.5)]
         self.assertEquals(results2, test_classifier.run(observations))
 
+    def test_run_with_determining(self):
+        # Test continue function evaluating to true
+        def function(_): return 0.7
+        test_determining_classifier = binary_classifier.BinaryClassifier("D001", function)
+
+        def function2(_): return 0.6
+        test_binary_classifier = binary_classifier.BinaryClassifier("002", function2)
+
+        def continue_function(x):
+            (_, value) = x
+            return value > 0.5
+        test_classifier = classifier.Classifier()
+
+        test_classifier.set_determining_classifier(test_determining_classifier, continue_function)
+        test_classifier.add_binary_classifier(test_binary_classifier)
+
+        observations = []
+        results = [("D001", 0.7), ("002", 0.6)]
+        self.assertEquals(results, test_classifier.run(observations))
+
+        # Test continue function evaluating to false
+        def function3(_): return 0.3
+        test_determining_classifier2 = binary_classifier.BinaryClassifier("D003", function3)
+        test_classifier.set_determining_classifier(test_determining_classifier2, continue_function)
+
+        observations = []
+        results = [("D003", 0.3)]
+        self.assertEquals(results, test_classifier.run(observations))
